34d2e879c7eefacd022a606492013078062a1fad
Anna Kohler <a.kohler.capa@gmail.com>
2022-05-18 16:20:59 -0400

    socket

---
+++ server/socket/index.js
@@ -0,0 +1,14 @@
+module.exports = io => {
+  io.on('connection', socket => {
+  console.log(socket.id, 'online')
+  socket.on('image-data', (data => {
+    socket.broadcast.emit('image-data', data)
+  }))
+})
+
+console.log('server environment', process.env.NODE_ENV)
+}
+
+
+
+//handle socket
--- webpack.config.js
+++ webpack.config.js
@@ -12,3 +12,35 @@ module.exports = {
     ]
   }
 };
+
+
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: ['babel-polyfill', './src/index.js'],
+//   output: {
+//     path: __dirname,
+//     filename: './public/bundle.js'
+//   },
+//   mode: 'development',
+//   devtool: 'source-map',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.jsx?$/,
+//         include: [
+//           path.resolve(__dirname, 'client')
+//         ],
+//         loader: 'babel-loader'
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ]
+//       }
+//     ]
+//   }
+// };
